101b76475b5051f938f7793db44e50c658b0507b
huangwei <huangwei@fanggeek.com>
2020-09-24 18:52:12 +0800

    移除 redis 的自动装配

--- src/main/java/com/yellow/k8s/warmup/Application.java
+++ src/main/java/com/yellow/k8s/warmup/Application.java
@@ -2,9 +2,10 @@ package com.yellow.k8s.warmup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableScheduling
 public class Application {
 

8310054bf79b0eb6c95b4cd2055e766258322b87
huangwei <huangwei@fanggeek.com>
2020-09-25 09:32:31 +0800

    新增注解 EnableReactiveMongoRepositories

--- src/main/java/com/yellow/k8s/warmup/Application.java
+++ src/main/java/com/yellow/k8s/warmup/Application.java
@@ -3,10 +3,12 @@ package com.yellow.k8s.warmup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableScheduling
+@EnableReactiveMongoRepositories
 public class Application {
 
     public static void main(String[] args) {
